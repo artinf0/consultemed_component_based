8e346602ed5886c963244896023f3164aa0a66d0
Sergio Diniz <sergiodinizsh@gmail.com>
2019-08-17 08:43:07 -0300

    insert admin

--- sql/insert.sql
+++ sql/insert.sql
@@ -1,4 +1,1 @@
-INSERT INTO TB_USUARIOS (nome, CPF, email, telefone, login, senha) VALUES ('Carlos','65487321','carlos@g.com','83987456321','cbgomes','123');
-INSERT INTO TB_USUARIOS (nome, CPF, email, telefone, login, senha) VALUES ('Bel','65443321','bel@g.com','8398745551','bel','123');
-INSERT INTO TB_USUARIOS (nome, CPF, email, telefone, login, senha) VALUES ('sergio','65480921','sergio@g.com','83976556321','sergio','123');
-INSERT INTO TB_USUARIOS (nome, CPF, email, telefone, login, senha) VALUES ('arthur','65127321','artinfo@g.com','83912356321','artinfo','123');
+INSERT INTO TB_USUARIOS (nome, CPF, email, telefone, senha) VALUES ('Admin','65487321','admin','83987456321','21232F297A57A5A743894A0E4A801FC3');
